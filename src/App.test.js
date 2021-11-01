6d483032cf4a1b99469375f72fc14556e235d6dc
yanjiangnan <yanjiangnan@baidu.com>
2021-11-01 14:34:07 +0800

    FIX: test update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('firstLaunch', () => {
+    render(<App />);
+    const firstLaunch = screen.getByText(/Be the first to know when we launch/i);
+    expect(firstLaunch).toBeInTheDocument();
 });
